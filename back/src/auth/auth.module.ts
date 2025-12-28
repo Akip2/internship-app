48d69e269fa31f198cc62bdacd9c83389e8fd7de
Antoine FONTANEZ <antoinef252@gmail.com>
2025-12-28 15:37:38 +0100

    jwt guard + token

--- back/src/auth/auth.module.ts
+++ back/src/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
 import { JwtModule } from "@nestjs/jwt";
 import { ConfigModule, ConfigService } from "@nestjs/config";
+import { JwtAuthGuard } from "./jwt-auth.guard";
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { ConfigModule, ConfigService } from "@nestjs/config";
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtAuthGuard],
+  exports: [JwtAuthGuard]
 })
 export class AuthModule { }
---
+++ back/src/auth/jwt-auth.guard.ts
@@ -0,0 +1,39 @@
+import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtAuthGuard implements CanActivate {
+  constructor(
+    private jwtService: JwtService,
+    private configService: ConfigService,
+  ) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    
+    const authHeader = request.headers.authorization;
+    
+    if (!authHeader) {
+      throw new UnauthorizedException('Token manquant');
+    }
+    
+    const token = authHeader.replace('Bearer ', '');
+    
+    try {
+      const payload = this.jwtService.verify(token, {
+        secret: this.configService.get('JWT_SECRET'),
+      });
+      
+      request.user = {
+        userId: payload.userId,
+        role: payload.role,
+      };
+      
+      return true;
+      
+    } catch (error) {
+      throw new UnauthorizedException('Token invalide ou expiré');
+    }
+  }
+}
