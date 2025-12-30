c6643be95797fb944c7d93b22a19456241ce7c3d
Antoine FONTANEZ <antoinef252@gmail.com>
2025-12-30 17:10:20 +0100

    mail system

--- back/src/accounts/accounts.module.ts
+++ back/src/accounts/accounts.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 import { AccountsService } from "./accounts.service";
 import { AccountsController } from "./accounts.controller";
 import { AuthModule } from "src/auth/auth.module";
+import { MailModule } from "src/mail/mail.module";
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, MailModule],
   controllers: [AccountsController],
   providers: [AccountsService],
 })
